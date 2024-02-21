88cb18fa517090ed7092aedfdc22158f0e6bf437
agilecollab <james.pike@agilecollab.ca>
2024-02-20 18:52:29 -0700

    added defines for esp32, and imports for necessary libs

--- aws_iot_replay_8266.ino
+++ aws_iot_replay_8266.ino
@@ -1,14 +1,29 @@
-#include <ESP8266WiFi.h>
-#include <WiFiManager.h>
+// Conditional compilation to include platform-specific libraries
+#include "secrets.h"
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <ESP8266WiFiMulti.h>
+  #include <WiFiClientSecureBearSSL.h> // Use the correct include for ESP8266
+  using namespace BearSSL;  // Use BearSSL namespace
+  std::unique_ptr<BearSSL::WiFiClientSecure> net(new BearSSL::WiFiClientSecure); // Correctly declare the net object
+  std::unique_ptr<BearSSL::X509List> cert(new BearSSL::X509List(cacert));
+  std::unique_ptr<BearSSL::X509List> client_crt(new BearSSL::X509List(client_cert));
+  std::unique_ptr<BearSSL::PrivateKey> key(new BearSSL::PrivateKey(privkey));
+  BearSSL::PrivateKey key(privkey);
+  WiFiClientSecure net;
+#elif defined(ESP32)
+    #include <WiFi.h>
+  #include <WiFiClientSecure.h> // Make sure this is included for ESP32
+  std::unique_ptr<WiFiClientSecure> net(new WiFiClientSecure); // Correctly declare the net object for ESP32
+#endif
+
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-#include <time.h>
-#include "secrets.h"
-
 #define TIME_ZONE -7 //MST
-#define RELAY_PIN D1
+#define RELAY_PIN 1
 
 #define AWS_IOT_PUBLISH_TOPIC   "esp8266/pub" // Publish state here
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp8266/sub" // Subscibe to commands here
@@ -17,15 +32,9 @@ bool relayState = false;
 unsigned long lastMillis = 0;
 unsigned long previousMillis = 0;
 const long interval = 5000;
+ 
+PubSubClient client(*net.get()); 
 
-WiFiClientSecure net;
- 
-BearSSL::X509List cert(cacert);
-BearSSL::X509List client_crt(client_cert);
-BearSSL::PrivateKey key(privkey);
- 
-PubSubClient client(net);
- 
 time_t now;
 time_t nowish = 1510592825;
  
@@ -76,9 +85,15 @@ void connectAWS() {
  
   NTPConnect();
  
-  net.setTrustAnchors(&cert);
-  net.setClientRSACert(&client_crt, &key);
- 
+  #if defined(ESP8266)
+    net->setTrustAnchors(&cert);
+    net->setClientRSACert(&client_crt, &key);
+  #elif defined(ESP32)
+    net->setCACert(cacert);
+    net->setCertificate(client_cert);
+    net->setPrivateKey(privkey);
+  #endif
+
   client.setServer(MQTT_HOST, 8883);
   client.setCallback(messageReceived);
  
@@ -113,7 +128,7 @@ void publishMessage() {
  
  
 void setup() {
-  Serial.begin(460800);
+  Serial.begin(115200);
   
   // Setup Relay Pin
   pinMode(RELAY_PIN, OUTPUT);
@@ -125,7 +140,7 @@ void setup() {
   // If it does not connect it starts an access point with the specified name
   // Here "AutoConnectAP" is the name of the AP that will be created if needed
   // and goes into a blocking loop awaiting configuration
-  wifiManager.autoConnect("Relay ESP8266 MQTT");
+  wifiManager.autoConnect("Relay ESP MQTT");
   
   // If we get here, we are connected to the WiFi
   Serial.println("Connected to WiFi");
